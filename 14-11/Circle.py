3e6be7eeeaaa7e1b016f6b61369161afe1733ea4
mr <berestovilya77@gmail.com>
2022-11-17 13:58:14 +0300

    Upload homework

---
+++ 14-11/Circle.py
@@ -0,0 +1,45 @@
+"""Файл с классом."""
+
+import math
+
+class NotValidCircle(Exception):
+    """Такого круга не существует."""
+    pass
+
+class Circle:
+    '''Класс для геометрической фигуры - круг.'''
+
+    def __init__(self, radius: float) -> None:
+        """Инициализирует круг.
+        Args:
+            radius: float - радиус круга.
+        Raises:
+            NotValidCircle: можно ли построить круг.
+        """
+        self.radius = radius
+        if not self.is_valid():
+            raise NotValidCircle
+
+    def dlina(self) -> float:
+        """Считает длину круга.
+        Returns:
+            float: Длина окружности
+        """
+        return 2 * math.pi * self.radius
+    
+    def square(self) -> float:
+        """Считает площадь круга.
+        Returns:
+            float: Площадь круга
+        """
+        return math.pi * self.radius ** 2
+    def is_valid(self) -> bool:
+        """Проверка, существует ли такой круг.
+        Returns:
+            bool: результат проверки
+        """
+        if not isinstance(self.radius, (float)):
+            return False
+        elif self.radius <= 0:
+            return False
+        return True
---
+++ 14-11/Triangle.py
@@ -0,0 +1,52 @@
+"""Файл с классом."""
+
+class NotValidTriangle(Exception):
+    """Такого треугольника не существует."""
+    pass
+
+class Triangle:
+    '''Класс для геометрической фигуры - треугольник.'''
+
+    def __init__(self, side_a: float, side_b: float, side_c: float) -> None:
+        """Инициализирует трегольник.
+        Args:
+            side_a: float - длина первой стороны.
+            side_b: float - длина второй стороны.
+            side_c: float - длина третьей стороны.
+        Raises:
+            NotValidTriangle: можно ли построить треугольник.
+        """
+        self.side_a = side_a
+        self.side_b = side_b
+        self.side_c = side_c
+        if not self.is_valid():
+            raise NotValidTriangle
+ 
+    def find_square(self) -> float:
+        """Считает площадь треугольника.
+        Returns:
+            float: Площадь треугольника
+        """
+        p = (self.side_a + self.side_b + self.side_c) / 2
+        return (p*(p - self.side_a)*(p - self.side_b)*(self.side_c))**0.5 
+
+    def find_perimetr(self) -> float:
+        """Считает периметр треугольника.
+        Returns:
+            float: Периметр треугольника
+        """
+        return self.side_a + self.side_b + self.side_c
+    def is_valid(self) -> bool:
+        """Проверка, существует ли такой треугольник.
+        Returns:
+            bool: результат проверки
+        """
+        sides = sorted([self.side_a, self.side_b, self.side_c], reverse=True)
+        for side in sides:
+            if not isinstance(side, float):
+                return False
+            if side <= 0:
+                return False
+        if sides[0] > sides[1] + sides[2]:
+            return False
+        return True
