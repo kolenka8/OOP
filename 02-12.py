5cd2a192e3d5a152c7d01f24f3747029d01f8769
mr <berestovilya77@gmail.com>
2022-12-26 13:26:24 +0300

    Update homework

--- 02-12.py
+++ 02-12.py
@@ -7,28 +7,28 @@ class Philosopher(Process):
     THINK_INTERVAL = (5, 6)
     EAT_INTERVAL = (5, 6)
 
-    def __init__(self, name, l_lock, r_lock):
+    def __init__(self, name, left_lock, right_lock):
         super().__init__(name=name)
-        self.l_lock = l_lock
-        self.r_lock = r_lock
+        self.left_lock = left_lock
+        self.right_lock = right_lock
 
     def eating(self):
-        print('Philosopher {0} is eating'.format(self.name))
+        print('Философ {0} кушает'.format(self.name))
         sleep(randint(*Philosopher.EAT_INTERVAL))
-        print('Philosopher {0} end eating and start thinking.'.format(self.name))
-        self.r_lock.release()
-        self.l_lock.release()
-        print('Philosopher {0} is thinking'.format(self.name))
+        print('Философ {0} закончил кушать и начинает думать.'.format(self.name))
+        self.right_lock.release()
+        self.left_lock.release()
+        print('Философ {0} думает'.format(self.name))
         sleep(randint(*Philosopher.THINK_INTERVAL))
-        print('Philosopher {0} end thinking and want`s eating.'.format(self.name))
+        print('Философ {0} закончил думать и хочет кушать.'.format(self.name))
 
     def run(self):
         while True:
-            if self.l_lock.acquire():
-                if self.r_lock.acquire():
+            if self.left_lock.acquire():
+                if self.right_lock.acquire():
                     self.eating()
                 else:
-                    self.l_lock.release()
+                    self.left_lock.release()
 
 
 class Eat:
@@ -36,7 +36,6 @@ class Eat:
         self.chopsticks = chopsticks
         for num_phil in range(5):
             Philosopher(str(num_phil), self.chopsticks[num_phil - 1], self.chopsticks[num_phil]).start()
-
 
 if __name__ == '__main__':
     chopsticks = []
